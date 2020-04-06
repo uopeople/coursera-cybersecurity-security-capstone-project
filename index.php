45d0cf97dca27a0b94f554c7540bb1050a4660e6
Daniel Petrescu <>
2020-04-06 11:34:38 +1000

    Added sample index.html

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Coursera Security Capstone (test)</title>
+    <meta name="author" content="Daniel">
+</head>
+
+<body>
+    <p>Hello</p>
+</body>
+
+</html>
