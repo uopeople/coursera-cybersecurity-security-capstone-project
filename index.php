ea0a61a66a8ddd4985ead020bfe8f33d53e226aa
Daniel Petrescu <>
2020-04-06 13:35:09 +1000

    Created main page

--- index.php
+++ index.php
@@ -1,15 +1,31 @@
-<!doctype html>
-
-<html lang="en">
+<!DOCTYPE html>
+<html lang=en>
 
 <head>
-    <meta charset="utf-8">
-    <title>Coursera Security Capstone (test)</title>
-    <meta name="author" content="Daniel">
+    <meta http-equiv=Content-Type content="text/html; charset=utf-8">
+    <meta name=viewport content="width=device-width, initial-scale=1" />
+
+    <meta name=author content="Daniel Petrescu">
+    <title>Messaging System</title>
+
+    <!-- CSS -->
+    <link rel=stylesheet media=all href=css/index.css>
+
+    <!-- Fonts -->
+    <link rel=stylesheet href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
 </head>
 
 <body>
-    <p>Hello</p>
+    <div>
+        <div id="header">
+            <i class="fas fa-comments"></i>
+            <h1>Coursera Capstone Project Messaging System</h1>
+        </div>
+        <div id="main-links">
+            <p id="login">Log in</p>
+            <p id="register">Register</p>
+        </div>
+    </div>
 </body>
 
 </html>
