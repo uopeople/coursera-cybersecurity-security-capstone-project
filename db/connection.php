8eadec4494c3c744dd239762a19f554f65cf9070
Daniel Petrescu <>
2020-04-15 16:32:22 +1000

    Moved the PDO creation to a function
    This will typically be called after this file has been included/required by another file.

--- db/connection.php
+++ db/connection.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Connect to the database and return a PDO.
+ *
+ * @return A PDO that can be used to interact with the database.
+ */
+function get_db_pdo()
+{
     // Get the database URL from the environment
     $db = parse_url(getenv("DATABASE_URL"));
     if(empty($db["path"])) {
@@ -21,4 +28,7 @@
         http_response_code(500);
         exit();
     }
+
+    return $pdo;
+}
 ?>
