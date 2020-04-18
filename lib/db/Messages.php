330bcfee334bc16818256c2be9f3698b420e9111
Giuseppe Arcidiacono <arcidiaconog@gmail.com>
2020-04-18 21:01:09 +0000

    Add new file (Messages.php)

---
+++ lib/db/Messages.php
@@ -0,0 +1,142 @@
+<?php
+
+
+namespace lib\db;
+
+use lib\model\Message;
+use PDO;
+
+/**
+ * Access to database table 'messages'
+ */
+class Messages
+{
+
+    /**
+     * @var PDO
+     */
+    private $pdo;
+
+
+    public function __construct()
+    {
+        $this->pdo = Connection::get_db_pdo();
+    }
+
+    /**
+     * Returns the message with the given message id. If no such message exists, this function returns null.
+     *
+     * @param int $messageId
+     *
+     * @return Message|null
+     */
+    public function loadMessageById(int $messageId): ?Message
+    {
+        
+        $sql = 'SELECT id, sender, recipient, message, date, read FROM messages WHERE id = ?';
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute([$messageId]);
+        $message = $stmt->fetch(PDO::FETCH_ASSOC);
+        if (!message) {
+            // not found
+            return null;
+        }
+        return $this->createMessageEntityFromDbRecord(message);
+    }
+
+
+    /**
+     * Returns messages whith the given sender. If no such message exists, this function returns null.
+     * 
+     * @param string $senderId
+     *
+     * @return Message|null
+     */
+    public function loadMessagesBySender(int $senderId): ?Message
+    {
+        $sql = 'SELECT id, sender, recipient, message, date, read FROM messages WHERE sender = ?';
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute([$senderId]);
+        $message = $stmt->fetch(PDO::FETCH_ASSOC);
+        if (!$message) {
+            // not found
+            return null;
+        }
+        return $this->createMessageEntityFromDbRecord($message);
+    }
+
+   /**
+     * Returns messages whith the given recipient. If no such message exists, this function returns null.
+     * 
+     * @param string $recipientId
+     *
+     * @return Message|null
+     */
+    public function loadMessagesByRecipient(int $recipientId): ?Message
+    {
+        $sql = 'SELECT id, sender, recipient, message, date, read FROM messages WHERE recipient = ?';
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute([$recipientId]);
+        $message = $stmt->fetch(PDO::FETCH_ASSOC);
+        if (!$message) {
+            // not found
+            return null;
+        }
+        return $this->createMessageEntityFromDbRecord($message);
+    }
+
+    /**
+     * Checks if a given message is read or not.
+     *
+     * @param Message $message The message that should be checked. Can be loaded via `loadMessageByMessagename` or similar methods.
+     *
+     * @return bool
+     */
+    public function isMessageRead(Message $message, int $lockDurationSeconds)
+    {
+        $now = time();
+        $messageLockedSince = $message->getLockedTime();
+        if ($messageLockedSince === null) {
+            // message is not locked
+            return false;
+        }
+        if ($messageLockedSince + $lockDurationSeconds < $now) {
+            // message was locked, but lock duration has expired...
+            return false;
+        }
+        return true;
+    }
+
+    public function insertNewMessage(int $sender, int $recipient, string $message, string $date, bool $read)
+    {
+          
+        $sql = 'INSERT INTO messages (sender, recipient, message, date, read) VALUES (?, ?, ?, ?, ?)';
+
+       
+        $stmt = $this->pdo->prepare($sql);
+        try {
+            $stmt->execute([$sender, $recipient, $message,$date,$read]);
+            $cnt = $stmt->rowCount();
+            if ($cnt < 1) {
+                return false;
+            }
+            return true;
+        } catch (\PDOException $e) {
+            return false;
+        }
+    }
+
+    
+    private function createMessageEntityFromDbRecord(array $dbRecord): Message
+    {
+         return new Message(
+            intval($dbRecord['id']),
+            intval($dbRecord['sender']),
+            intval($dbRecord['recipient']),
+            $dbRecord['message'],
+            $dbRecord['date'],
+            boolval($dbRecord['read'])
+            
+        );
+    }
+}
