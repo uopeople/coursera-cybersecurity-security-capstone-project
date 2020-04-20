bd40440d3f6a3ea2991f3e8765eca4c2d23f3710
Daniel Petrescu <>
2020-04-20 12:32:30 +1000

    Added further validation checks for registration field values
    * Username: Valid characters, max length
    * Email address: Valid address, max length
    * Password: max length

--- lib/service/RegistrationFormValidation.php
+++ lib/service/RegistrationFormValidation.php
@@ -4,9 +4,15 @@ namespace lib\service;
 
 class RegistrationFormValidation {
 
+    /**
+     * The username and email will be remembered if validation fails.
+     */
     public static $username = "";
     public static $email = "";
 
+    /**
+     * The error messages to show for each field if validation fails.
+     */
     public static $usernameErr = "";
     public static $emailErr = "";
     public static $passErr = "";
@@ -30,22 +36,46 @@ class RegistrationFormValidation {
     ): bool {
         $values_ok = TRUE;
 
+        // Check for empty username
         if (empty($username)) {
             self::$usernameErr = "Username is required";
             $values_ok = FALSE;
         } else {
             self::$username = $username;
+            // Check if username contains invalid characters
+            if(preg_match("/[^[:alnum:]\-_]/", $username)) {
+                self::$usernameErr = "Username must contain only: capital letters, lowercase letters, numbers, _, -";
+                $values_ok = FALSE;
+            // Check if username is too long
+            } elseif(strlen($username) > 255) {
+                self::$usernameErr = "Username cannot be longer than 255 characters";
+                $values_ok = FALSE;
+            }
         }
 
+        // Check for empty email address
         if (empty($email)) {
             self::$emailErr = "Email address is required";
             $values_ok = FALSE;
         } else {
             self::$email = $email;
+            // Check if this is a valid email address
+            if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                self::$emailErr = "Email address is not valid";
+                $values_ok = FALSE;
+            // Check if email is too long
+            } elseif(strlen($email) > 255) {
+                self::$emailErr = "Email address cannot be longer than 255 characters";
+                $values_ok = FALSE;
+            }
         }
 
         if (empty($password)) {
             self::$passErr = "Password is required";
+            $values_ok = FALSE;
+        } elseif(strlen($password) > 255) {
+            // Check if password is too long
+            self::$passErr = "Password cannot be longer than 255 characters";
             $values_ok = FALSE;
         }
 
--- tests/unit/lib/service/RegistrationFormValidationTest.php
+++ tests/unit/lib/service/RegistrationFormValidationTest.php
@@ -14,6 +14,17 @@ class RegistrationFormValidationTest extends TestCase
     private static $TEST_PASS = 'pass';
 
     /**
+     * Reset all error message fields of RegistrationFormValidation.
+     * @after
+     */
+    public function resetErrorMessages() {
+        RegistrationFormValidation::$usernameErr = "";
+        RegistrationFormValidation::$emailErr = "";
+        RegistrationFormValidation::$passErr = "";
+        RegistrationFormValidation::$passRptErr = "";
+    }
+
+    /**
      * Fail validation if the username was not entered.
      */
     public function test_validateValues_returns_false_on_empty_username() {
@@ -21,8 +32,26 @@ class RegistrationFormValidationTest extends TestCase
 
         $this->assertFalse($values_ok, 'Validation was expected to return false');
         $this->assertNotEmpty(RegistrationFormValidation::$usernameErr, "usernameErr should not be blank");
+    }
 
-        RegistrationFormValidation::$usernameErr = "";
+    /**
+     * Fail validation if the username was too long.
+     */
+    public function test_validateValues_returns_false_on_long_username() {
+        $values_ok = RegistrationFormValidation::validateValues(str_repeat("x", 256), "", "", "");
+
+        $this->assertFalse($values_ok, 'Validation was expected to return false');
+        $this->assertNotEmpty(RegistrationFormValidation::$usernameErr, "usernameErr should not be blank");
+    }
+
+    /**
+     * Fail validation if the username contained invalid characters.
+     */
+    public function test_validateValues_returns_false_on_username_with_invalid_characters() {
+        $values_ok = RegistrationFormValidation::validateValues("username=5", "", "", "");
+
+        $this->assertFalse($values_ok, 'Validation was expected to return false');
+        $this->assertNotEmpty(RegistrationFormValidation::$usernameErr, "usernameErr should not be blank");
     }
 
     /**
@@ -33,8 +62,52 @@ class RegistrationFormValidationTest extends TestCase
 
         $this->assertFalse($values_ok, 'Validation was expected to return false');
         $this->assertNotEmpty(RegistrationFormValidation::$emailErr, "emailErr should not be blank");
+    }
 
-        RegistrationFormValidation::$emailErr = "";
+    /**
+     * Fail validation if the email address was too long.
+     */
+    public function test_validateValues_returns_false_on_long_email() {
+        $values_ok = RegistrationFormValidation::validateValues(self::$TEST_USER, str_repeat("x", 256), "", "");
+
+        $this->assertFalse($values_ok, 'Validation was expected to return false');
+        $this->assertNotEmpty(RegistrationFormValidation::$emailErr, "emailErr should not be blank");
+    }
+
+    /**
+     * Generate a list of invalid email addresses for unit testing.
+     */
+    public function providerInvalidEmailAddress() {
+        return array(
+            ["email@example"],
+            ["email@-example.com"],
+            ["email@example..com"],
+            ["email.example.com"],
+            ["email.@example.com"],
+            ["email..email@example.com"],
+            [".email@example.com"],
+            ["email@example@example.com"],
+            ["email@111.222.333.44444"],
+            ["@example.com"],
+            ["plainaddress"],
+            ["email@example.com (Joe Smith)"],
+            ["Joe Smith <email@example.com>"],
+            ["#@%^%#$@#$@#.com"],
+            ["あいうえお@example.com"],
+            ["Abc..123@example.com"],
+            ['very.unusual."@".unusual.com@example.com\''],
+            ['very."(),:;<>[]".VERY."very@\\ "very".unusual@strange.example.com'],
+        );
+    }
+    /**
+     * Fail validation if the email address was invalid.
+     * @dataProvider providerInvalidEmailAddress
+     */
+    public function test_validateValues_returns_false_on_invalid_email(string $testEmail) {
+        $values_ok = RegistrationFormValidation::validateValues(self::$TEST_USER, $testEmail, "", "");
+
+        $this->assertFalse($values_ok, 'Validation was expected to return false');
+        $this->assertNotEmpty(RegistrationFormValidation::$emailErr, "emailErr should not be blank");
     }
 
     /**
@@ -45,8 +118,17 @@ class RegistrationFormValidationTest extends TestCase
 
         $this->assertFalse($values_ok, 'Validation was expected to return false');
         $this->assertNotEmpty(RegistrationFormValidation::$passErr, "passErr should not be blank");
+    }
 
-        RegistrationFormValidation::$passErr = "";
+    /**
+     * Fail validation if the password was too long.
+     */
+    public function test_validateValues_returns_false_on_long_password() {
+        $values_ok = RegistrationFormValidation::validateValues(self::$TEST_USER, self::$TEST_EMAIL,
+                                                                str_repeat("x", 256), "");
+
+        $this->assertFalse($values_ok, 'Validation was expected to return false');
+        $this->assertNotEmpty(RegistrationFormValidation::$passErr, "passErr should not be blank");
     }
 
     /**
@@ -58,8 +140,6 @@ class RegistrationFormValidationTest extends TestCase
 
         $this->assertFalse($values_ok, 'Validation was expected to return false');
         $this->assertNotEmpty(RegistrationFormValidation::$passRptErr, "passRptErr should not be blank");
-
-        RegistrationFormValidation::$passRptErr = "";
     }
 
     /**
@@ -71,15 +151,35 @@ class RegistrationFormValidationTest extends TestCase
 
         $this->assertFalse($values_ok, 'Validation was expected to return false');
         $this->assertNotEmpty(RegistrationFormValidation::$passRptErr, "passRptErr should not be blank");
-
-        RegistrationFormValidation::$passRptErr = "";
     }
 
     /**
-     * Successful validation.
+     * Generate a list of valid email addresses for unit testing.
      */
-    public function test_validateValues_returns_true_on_successful_validation() {
-        $values_ok = RegistrationFormValidation::validateValues(self::$TEST_USER, self::$TEST_EMAIL,
+    public function providerValidEmailAddress() {
+        return array(
+            ["email@example.com"],
+            ["firstname.lastname@example.com"],
+            ["email@subdomain.example.com"],
+            ["firstname+lastname@example.com"],
+            ["email@[123.123.123.123]"],
+            ['"email"@example.com'],
+            ["1234567890@example.com"],
+            ["email@example-one.com"],
+            ["_______@example.com"],
+            ["email@example.name"],
+            ["email@example.museum"],
+            ["email@example.co.jp"],
+            ["firstname-lastname@example.com"],
+            ['much."more\ unusual"@example.com'],
+        );
+    }
+    /**
+     * Successful validation.
+     * * @dataProvider providerValidEmailAddress
+     */
+    public function test_validateValues_returns_true_on_successful_validation(string $testEmail) {
+        $values_ok = RegistrationFormValidation::validateValues(self::$TEST_USER, $testEmail,
                                                                 self::$TEST_PASS, self::$TEST_PASS);
 
         $this->assertTrue($values_ok, 'Validation was expected to return true');
